dc7ee17206957a121971982bddbb6476a6f47297
Aniket1997 <16riju@gmail.com>
2025-01-01 20:57:33 +0530

    added update tasks and delete tasks

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -1,3 +1,4 @@
+const task = require('../model/task');
 const tasks = require('../model/task');
 
 // Create a new task
@@ -39,3 +40,52 @@ exports.deleteTask = async (req, res) => {
     return res.status(200).json({ status: 'Success', message: 'Task deleted successfully' });
 };
 
+exports.updateTask = async (req,res)=>{
+    try{
+        const { taskId, title, description, board, assignedTo, status } = req.body;
+
+    if(!taskId)
+    {
+        return res.status(400).json({status:'Failure',message:'Task Id is required'});
+    }
+
+    const updatedTask = await tasks.findByIdAndUpdate(
+        taskId,
+        { title, description, board, assignedTo, status },
+        { new: true, runValidators: true }
+    )
+    
+    if(!updatedTask)
+    {
+        return res.status(200).json({status:'Success',message:'Task not found'});
+    }
+
+    return res.status(200).json({status:'Success',message:'Task updated successfully'},updatedTask);
+    }catch(error)
+    {
+        return res.status(500).json({status:'Failure',message:'Internal Server error'});
+    }
+}
+
+exports.getTasks = async (req,res)=>
+{
+    try{
+        const {taskId} = req.params;
+        if(!taskId)
+        {
+            const tasks = await tasks.find();
+            return res.status(200).json({status:'Success',message:'Tasks fetch successfully',tasks});
+        }
+        else{
+            const task = await tasks.findById(taskId);
+            if(!task)
+            {
+                return res.status(200).json({status:'Success',message:'Task Not Found with the provided id',task:[]});
+            }
+            return res.status(200).json({status:'Success',message:'Tasks Fetch Successfully',task:[]});
+        }
+    }catch(err)
+    {
+        return res.status(500).json({status:'Failure',message:'Internal Server error'});
+    }
+}
--- server/router/taskRouter.js
+++ server/router/taskRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createTask, deleteTask } = require('../controller/taskController');
+const { createTask, deleteTask, updateTask, getTasks } = require('../controller/taskController');
 const router=express.Router();
 
 router.post("/create-task",createTask);
 router.delete("/delete-task",deleteTask);
+router.put("/update-task",updateTask);
+router.get("/tasks/:taskId?",getTasks);
 
 module.exports=router;
